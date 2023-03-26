a68a6b1ff4ad3dc35959a9ff590d983b05a3e8b8
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-03-26 21:33:57 +0530

    Create Reverse.java

---
+++ Array/Reverse.java
@@ -0,0 +1,27 @@
+class Reverse
+{
+	public static void main(String args[])
+	{
+		int a[]={2,4,8,9};
+		for(int i=0;i<a.length;i++)
+		System.out.print(a[i] + " ");
+		a=B.reverse(a);
+		System.out.println("\n reversed array -");
+		for(int i=0;i<a.length;i++)
+		System.out.print(a[i] + " ");
+	}
+}
+
+class B
+{
+	static int[] reverse(int a[])
+	{	
+		int l=a.length-1;
+		int x[]=new int[a.length];
+		for(int i=0;i<a.length;i++)
+		{
+			x[i]=a[l--];
+		}
+		return x;
+	}
+}
