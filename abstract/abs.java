bfa4b30a826cde26319f8e33fe246625f367ddf8
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-04-22 10:09:21 -0700

    Update abs.java

--- abstract/abs.java
+++ abstract/abs.java
@@ -1,3 +1,12 @@
-class abs
-{
-}
+A class which is declared with the abstract keyword is known as an abstract class in Java. It can have abstract and non-abstract methods (method with the body).
+Abstraction is a process of hiding the implementation details and showing only functionality to the user.
+
+There are two ways to achieve abstraction in java :
+1>Abstract class (0 to 100%)
+2>Interface (100%)
+
+A method which is declared as abstract and does not have implementation is known as an abstract method.
+ex: abstract void show();
+
+Notes:
+If there is an abstract method in a class, that class must be abstract.
