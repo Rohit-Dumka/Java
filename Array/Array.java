29c5a5da7a67dfb143cc004b277e56c71d2ad2eb
Rohit Dumka <96404582+Rohit-Dumka@users.noreply.github.com>
2023-05-10 09:36:33 -0700

    Create Array.java

---
+++ Array/Array.java
@@ -0,0 +1,16 @@
+// find minimum of an array
+class str{  
+    static void min(int arr[]){  
+        int min=arr[0];  
+        for(int i=1;i<arr.length;i++)  
+            if(min>arr[i])  
+            min=arr[i];  
+      
+        System.out.println(min);  
+    }  
+      
+    public static void main(String args[]){  
+        int a[]={33,3,4,5,-3,-13,0};
+        min(a);
+    }
+}  
